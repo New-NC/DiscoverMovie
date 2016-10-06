ec6df5a245338cab8e5dcb8805bba02e14f5fdcf
CarolPC <carolina.pascale.c@gmail.com>
2016-10-06 17:10:19 -0300

    Tela inicial com funcionalidadde e primeira tela de questao sem funcionalidade

---
+++ app/src/main/java/newnc/github/io/discovermovie/FirstQuestion.java
@@ -0,0 +1,13 @@
+package newnc.github.io.discovermovie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstQuestion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_question);
+    }
+}
--- app/src/main/java/newnc/github/io/discovermovie/MainActivity.java
+++ app/src/main/java/newnc/github/io/discovermovie/MainActivity.java
@@ -1,7 +1,10 @@
 package newnc.github.io.discovermovie;
 
+import android.content.Intent;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void startQuestionnaire(View view) {
+        Intent myIntent = new Intent(view.getContext(), FirstQuestion.class); /** Class name here */
+        startActivityForResult(myIntent, 0);
+    }
 }
+
+
