3f5aeee90082a8514bbb6186d4ff27804665b363
Filipe Santos Rocchi <filipe.rocchi@gmail.com>
2016-11-29 09:54:54 -0200

    Mudando backgroud default das capas para invisivel
    Mudança no carregamento da imagem também

--- app/src/main/java/io/newnc/discovermovie/task/DownloadImageTask.java
+++ app/src/main/java/io/newnc/discovermovie/task/DownloadImageTask.java
@@ -10,6 +10,7 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.RequestCreator;
 
 import io.newnc.discovermovie.view.Results;
 
@@ -17,7 +18,7 @@ import io.newnc.discovermovie.view.Results;
  * <code>AsyncTask</code> to download images.
  * Credits: http://stackoverflow.com/questions/2471935/how-to-load-an-imageview-by-url-in-android
  */
-public class DownloadImageTask extends AsyncTask<String, Void, String> {
+public class DownloadImageTask extends AsyncTask<String, Void, RequestCreator> {
     private Context context;
     private ImageView image;
     private ProgressBar progressBar;
@@ -38,8 +39,35 @@ public class DownloadImageTask extends AsyncTask<String, Void, String> {
     */
 
     @Override
-    protected String doInBackground(String... s) {
-        return s[0];
+    protected RequestCreator doInBackground(String... s) {
+
+        String url = IMG_URL + s[0];
+        RequestCreator a = null;
+
+        int rWidth, rHeight;
+        // w500 of TMDB is 500x750
+        rWidth = 500;
+        rHeight = 750;
+
+        if (context.getClass() == Results.class) {
+            log("Results"); // imagem maior
+            rWidth /= 2;
+            rHeight /= 2;
+        } else {
+            log("Not Results"); // imagem menor
+            rWidth /= 4;
+            rHeight /= 4;
+        }
+
+        try {
+            a = Picasso.with(context).load(url).resize(rWidth, rHeight);
+        }
+        catch(Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+
+        return a;
     }
 
     /**
@@ -47,24 +75,10 @@ public class DownloadImageTask extends AsyncTask<String, Void, String> {
      *
      */
     @Override
-    protected void onPostExecute(String s) {
+    protected void onPostExecute(RequestCreator a) {
         log("BEGIN onPostExecute");
 
-        String url = IMG_URL + s;
-        int rWidth, rHeight;
-        // w500 of TMDB is 500x750
-        rWidth = 500;
-        rHeight = 750;
-
-        if(context.getClass() == Results.class) {
-            log("Results");
-            Picasso.with(context).load(url).resize(rWidth / 2, rHeight / 2).into(image);
-        }
-        else {
-            log("Not Results");
-            Picasso.with(context).load(url).resize(rWidth / 3, rHeight / 3).into(image);
-        }
-
+        a.into(image);
         image.setVisibility(View.VISIBLE);
         progressBar.setVisibility(View.INVISIBLE);
 
